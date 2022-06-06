eb5afd606752ac71763112a7490d4192951e8adf
AlexisDanlos <alexis.danlos@epitech.eu>
2022-06-06 11:45:30 +0200

    removed useless data

--- db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-# delete role col in loginlinks table
-
-db = sqlite3.connect('dicord_bot\database.db')
-c = db.cursor()
-c.execute("ALTER TABLE loginlinks DROP COLUMN role")
-db.commit()
-db.close()
